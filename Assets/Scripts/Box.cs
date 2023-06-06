dae78da6b2ebdaa87d64f6dd65ca78c0eb8d496c
littlealex9999 <littlealex9999@gmail.com>
2023-06-06 16:34:27 +1000

    add box check mechanics

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script belongs on "Boxes" which check if items enter them and then can be used to find what items they hold
+/// </summary>
+public class Box : MonoBehaviour
+{
+    public List<PackItem> itemsInBox = new List<PackItem>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item") {
+            itemsInBox.Add(other.GetComponent<PackItem>());
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,7 @@ public class GameManager : MonoBehaviour
     float score;
 
     [SerializeField] Transform customerSpawnLocation;
+    [SerializeField] Transform customerWaitLocation;
     [SerializeField] int itemsPerPerson = 3;
 
     [SerializeField] GameObject[] customers;
@@ -38,15 +39,51 @@ public class GameManager : MonoBehaviour
 
     void CreateNewCustomer()
     {
+        // create a random customer
         Customer newCustomer = Instantiate(customers[Random.Range(0, customers.Length)]).GetComponent<Customer>();
         currentCustomers.Add(newCustomer);
 
         List<PackItem> newCustomerItems = new List<PackItem>();
 
         for (int i = 0; i < itemsPerPerson; ++i) {
+            // add random items to our list
             newCustomerItems.Add(requestableObjects[Random.Range(0, requestableObjects.Length)].GetComponent<PackItem>());
         }
 
         newCustomer.AssignItems(newCustomerItems);
+    }
+
+    bool CheckBoxDone(Box box)
+    {
+        foreach (Customer c in currentCustomers) {
+            List<int> usedItems = new List<int>();
+
+            for (int i = 0; i < c.items.Count; ++i) {
+                bool itemFound = false;
+
+                for (int j = 0; j < box.itemsInBox.Count; ++j) {
+                    if (usedItems.Contains(j)) continue;
+
+                    if (c.items[i].itemID == box.itemsInBox[j].itemID) {
+                        itemFound = true;
+                        usedItems.Add(j); // don't use the same object twice for checking if we have everything
+
+                        break;
+                    }
+                }
+
+                if (!itemFound) break; // we are missing one of the items meant to be in the box
+            }
+
+            if (usedItems.Count >= c.items.Count) { // we have all the items the customer wants
+                int unnecessaryItems = box.itemsInBox.Count - usedItems.Count;
+
+                // calculate score
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
