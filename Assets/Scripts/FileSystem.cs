496799cb74354c565681a9d980196be52d243e54
little <littlealex9999@gmail.com>
2023-06-12 15:52:04 +1000

    abstract FileSystem

--- Assets/Scripts/FileSystem.cs
+++ Assets/Scripts/FileSystem.cs
@@ -3,56 +3,88 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class FileSystem
 {
-    public void SaveFile(string filename)
+    #region Helper Functions
+    string DeterminePath(string filename) => Application.dataPath + "/" + filename;
+
+    BinaryWriter OpenWriter(string filename, out Stream stream)
     {
-        string path = Application.dataPath + "/" + filename;
+        string path = DeterminePath(filename);
 
         // either overwrites the old content, or creates a new file.
         FileMode mode = File.Exists(path) ? FileMode.Truncate : FileMode.CreateNew;
 
         // files get saved in the application directory
-        Stream s = File.Open(path, mode);
-        BinaryWriter bw = new BinaryWriter(s);
+        stream = File.Open(path, mode);
+        BinaryWriter bw = new BinaryWriter(stream);
 
 #if UNITY_EDITOR
         // only output this for debug purposes
         Debug.Log("Writing a file at: " + path);
 #endif
 
-        bw.Write(10);
-        
-        bw.Close();
-        s.Close();
+        return bw;
     }
 
-    public bool LoadFile(string filename, out int output)
+    BinaryReader OpenReader(string filename, out Stream stream)
     {
-        string path = Application.dataPath + "/" + filename;
+        string path = DeterminePath(filename);
 
         if (File.Exists(path)) {
-            Stream s = File.Open(path, FileMode.Open);
-            BinaryReader br = new BinaryReader(s);
+            stream = File.Open(path, FileMode.Open);
+            BinaryReader br = new BinaryReader(stream);
 
 #if UNITY_EDITOR
             // only output this for debug purposes
             Debug.Log("Reading a file at: " + path);
 #endif
 
-            output = br.ReadInt32();
-
-            return true;
+            return br;
         } else { // failed to load the file as it doesn't exist
 #if UNITY_EDITOR
             // only output this for debug purposes
             Debug.Log("Failed to find a file at: " + path);
 #endif
+        }
 
+        stream = null;
+        return null;
+    }
+
+    void CloseSystem(BinaryReader binaryIO, Stream stream)
+    {
+        binaryIO.Close();
+        stream.Close();
+    }
+    void CloseSystem(BinaryWriter binaryIO, Stream stream)
+    {
+        binaryIO.Close();
+        stream.Close();
+    }
+    #endregion
+
+    public void SaveFile(string filename)
+    {
+        BinaryWriter bw = OpenWriter(filename, out Stream s);
+
+        bw.Write(10);
+
+        CloseSystem(bw, s);
+    }
+
+    public bool LoadFile(string filename, out int output)
+    {
+        BinaryReader br = OpenReader(filename, out Stream s);
+        if (br != null) {
+            output = br.ReadInt32();
+
+            CloseSystem(br, s);
+            return true;
+        } else {
             output = 0;
-            return false; 
+            return false;
         }
     }
 }
