57e68fd24491b218d61a82c5c76797a202f66699
littlealex9999 <littlealex9999@gmail.com>
2023-06-13 15:34:21 +1000

    fix save errors

--- Assets/Scripts/PackItem.cs
+++ Assets/Scripts/PackItem.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PackItem : MonoBehaviour
 {
     [SerializeField] int ID; // items with the same ID will be considered the same for packing purposes
+    [SerializeField] new string name; // name only matters for display purposes
 
     public int itemID { get { return ID; } }
+    public string itemName { get { return name; } }
 }
