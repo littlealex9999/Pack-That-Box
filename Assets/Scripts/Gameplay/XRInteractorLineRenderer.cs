6bf08f03e36ced3e739c5b44a515775f888a1e50
littlealex9999 <littlealex9999@gmail.com>
2023-06-21 14:12:28 +1000

    attempt grab change

---
+++ Assets/Scripts/Gameplay/XRInteractorLineRenderer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(LineRenderer)), RequireComponent(typeof(XRRayInteractor))]
+public class XRInteractorLineRenderer : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+    XRRayInteractor rayInteractor;
+
+    Transform target;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        rayInteractor = GetComponent<XRRayInteractor>();
+    }
+
+    private void Update()
+    {
+        if (target != null) {
+            lineRenderer.SetPosition(0, rayInteractor.transform.position);
+            lineRenderer.SetPosition(1, target.position);
+        } else if (!lineRenderer.enabled) {
+            lineRenderer.enabled = false;
+        }
+    }
+
+    public void SetTarget(HoverEnterEventArgs args)
+    {
+        target = args.interactableObject.transform;
+    }
+
+    public void SetTarget(HoverExitEventArgs args)
+    {
+        target = null;
+    }
+}
